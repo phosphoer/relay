03d993a6ea12fac4333bc1c6d3e17dd51bece070
David Evans <david0evans@gmail.com>
2014-07-14 22:12:31 -0700

    Make build less sketchy

--- gulpfile.js
+++ gulpfile.js
@@ -7,83 +7,34 @@
   var zip = require('gulp-zip');
   var rimraf = require('gulp-rimraf');
 
-  var appFiles =
-  [
-    'package.json',
-    'index.html',
-    'ractive.js',
-    'style.css'
-  ];
-
-  var packageFiles =
-  [
-    'app/package.json',
-    'app/index.html',
-    'app/ractive.js',
-    'app/app.js',
-    'app/channel.js',
-    'app/user.js',
-    'app/events.js',
-    'app/commands.js',
-    'app/log.js',
-    'app/main.js',
-    'app/style.css'
-  ];
+  var modules = ['github', 'imgur', 'irc', 'underscore'];
 
   // Main build task
-  gulp.task('default', function(cb)
+  gulp.task('default', function()
   {
-    gulp.src('src/*.js')
-    .pipe(gulp.dest('app/'));
-
-    gulp.src(appFiles)
-    .pipe(gulp.dest('app/'));
-
-    setTimeout(function() {cb();}, 100);
-  });
-
-  gulp.task('clean', function(cb)
-  {
-    var cleanFiles = packageFiles.slice();
-    gulp.src(packageFiles)
+    gulp.src('bin/app/package.nw')
     .pipe(rimraf());
 
-    setTimeout(function() {cb();}, 100);
-  });
+    gulp.src('src/*.*')
+    .pipe(gulp.dest('bin/'))
+    .pipe(gulp.dest('bin/package'));
 
-  gulp.task('package', function(cb)
-  {
-    gulp.src(packageFiles)
-    .pipe(gulp.dest('app/package/'));
+    gulp.src('package.json')
+    .pipe(gulp.dest('bin/'))
+    .pipe(gulp.dest('bin/package'));
 
-    gulp.src('./node_modules/irc/**/*')
-    .pipe(gulp.dest('app/package/node_modules/irc/'));
+    for (var i = 0; i < modules.length; ++i)
+    {
+      gulp.src('node_modules/' + modules[i] + '/**/*')
+      .pipe(gulp.dest('bin/package/node_modules/' + modules[i]));
+    }
 
-    gulp.src('./node_modules/underscore/**/*')
-    .pipe(gulp.dest('app/package/node_modules/underscore/'));
-
-    gulp.src('./node_modules/imgur/**/*')
-    .pipe(gulp.dest('app/package/node_modules/imgur/'));
-
-    gulp.src('./node_modules/github/**/*')
-    .pipe(gulp.dest('app/package/node_modules/github/'));
-
-    setTimeout(function() {cb();}, 100);
-  });
-
-  gulp.task('release', ['default', 'package'], function(cb)
-  {
-    gulp.src('app/package/**/*')
+    setTimeout(function()
+    {
+    gulp.src('bin/package/**/*')
     .pipe(zip('package.nw'))
-    .pipe(gulp.dest('app/relay/'));
-
-    setTimeout(function() {cb();}, 100);
-  });
-
-  // Rerun the task when a file changes
-  gulp.task('watch', function()
-  {
-    gulp.watch(['src/*.js', 'index.html', 'package.json', 'style.css'], ['default']);
+    .pipe(gulp.dest('bin/app'));
+    }, 500);
   });
 
 })();
